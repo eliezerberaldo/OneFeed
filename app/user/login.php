460f7912f1d8d8ba642835bdf93c09f93e946a4e
eliezerberaldo <ribeiroeliezer48@gmail.com>
2025-10-28 11:11:47 -0300

    AB#16

--- app/user/login.php
+++ app/user/login.php
@@ -2,7 +2,8 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Entrar - OneFeed</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar-se - OneFeed</title>
     <link rel="stylesheet" href="../../public/css/Style_Cad.css">
     <link rel="shortcut icon" href="../../public/img/favicon.ico">
 </head>
@@ -16,25 +17,35 @@
         </aside>
 
         <main class="formulario-principal">
-            <h1>Entrar</h1>
+            <h1>Cadastrar-se</h1>
             
-            <form id="formulario-login">
+            <form id="formulario-cadastro">
                 
                 <div class="grupo-formulario">
                     <label for="email">E-mail:</label>
                     <input type="email" id="email" name="email" required placeholder="seuemail@exemplo.com">
                 </div>
-
+                
+                <div class="grupo-formulario">
+                    <label for="usuario">Nome de Usuário:</label>
+                    <input type="text" id="usuario" name="usuario" required placeholder="Seu nome de usuário">
+                </div>
+                
                 <div class="grupo-formulario">
                     <label for="password">Senha:</label>
-                    <input type="password" id="password" name="password" required placeholder="Sua senha">
+                    <input type="password" id="password" name="password" required placeholder="Crie uma senha forte">
                 </div>
-
+                
+                <div class="grupo-formulario">
+                    <label for="confirm-password">Confirmar Senha:</label>
+                    <input type="password" id="confirm-password" name="confirm-password" required placeholder="Confirme sua senha">
+                </div>
+                
                 <button type="submit" class="botao-enviar">Confirmar</button>
             </form>
-            
+
             <p class="link-cadastro">
-                Não possui uma conta? <a href="register.php">Cadastre-se</a>
+                Já possui uma conta? <a href="login.php">Entre</a>
             </p>
 
         </main>
